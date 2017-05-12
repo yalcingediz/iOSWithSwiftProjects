27ddece258ce04c31b3739d2add43d93ce479c8a
Berkant GEDIZ <yalcingediz@gmail.com>
2017-05-11 19:26:57 -0700

    File inception

---
+++ ScrollViewExample/ScrollViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewExample
+//
+//  Created by Berkant Y. GEDIZ on 5/11/17,19.
+//  Copyright © 2017 Mnemosyne C2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
