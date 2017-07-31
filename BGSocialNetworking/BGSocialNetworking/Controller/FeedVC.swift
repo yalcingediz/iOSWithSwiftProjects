9ff62b148d328f69a22cf88df1843aa285104a5c
Berkant GEDIZ <yalcingediz@gmail.com>
2017-07-31 10:09:14 -0700

    Firebase set up, competion of UI for Feed

--- BGSocialNetworking/BGSocialNetworking/Controller/FeedVC.swift
+++ BGSocialNetworking/BGSocialNetworking/Controller/FeedVC.swift
@@ -10,14 +10,35 @@ import UIKit
 import FirebaseAuth
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
-
+class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
+    // Implement for UITableViewDataSource: START
+    // numberOfSections(in tableView is normally optional and when it not implemented it defaul;t section is 1 anyway
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TABLE_VIEW_CELL_ID, for: indexPath)
+        
+        return cell
+    }
+     // Implement for UITableViewDataSource: END
+    
     @IBAction func singOutButtonPressed(_ sender: UIButton) {
         let firebaseAuth = Auth.auth()
         if KeychainWrapper.standard.removeObject(forKey: KEY_UID) != true {
---
+++ BGSocialNetworking/BGSocialNetworking/View/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  BGSocialNetworking
+//
+//  Created by Berkant Y. GEDIZ on 7/24/17,30.
+//  Copyright © 2017 Mnemosyne C2. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
